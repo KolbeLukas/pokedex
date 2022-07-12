ee1db0cc24119db9d576358080e96905281d78d6
Lukas Kolbe <kolbe.lukas@gmx.de>
2022-07-13 00:04:18 +0200

    fixed scroll

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let pokemonData = [];
+let pokemonSpecies = [];
 let LIMIT = 50;
 let offset = 0;
 
@@ -12,35 +13,45 @@ async function init() {
 
 async function loadPokemonData(allPokemon) {
     for (let i = offset; i < offset + LIMIT; i++) {
-        let url = allPokemon['results'][i]['url'];
-        let response = await fetch(url);
-        let pokemonData = await response.json();
-        renderOnePokemon(i, pokemonData);
+        let urlData = allPokemon['results'][i]['url'];
+        let responseData = await fetch(urlData);
+        let data = await responseData.json();
+        pokemonData.push(data);
+        let urlSpecies = pokemonData[i]['species']['url'];
+        let response = await fetch(urlSpecies);
+        let species = await response.json();
+        pokemonSpecies.push(species);
+    }
+    document.getElementById('b').classList.remove('n-scroll');
+    renderOnePokemon();
+}
+
+
+function renderOnePokemon() {
+    for (let i = offset; i < offset + LIMIT; i++) {
+        const onePokemon = pokemonData[i];
+        
+    document.getElementById('container').innerHTML += /*html*/`
+        <div id="pokemon-card-small${i}" class="pokemon-card-small bg-small">
+            <div class="pokemon-card-top">
+                <h2 class="fit">${onePokemon['name']}</h2>
+                <span class="fit pokemon-id">#${onePokemon['id']}</span>
+            </div>
+            <div class="pokemon-card-bottom">
+                <div id="pokemon-types${i}" class="pokemon-types fit"></div>
+                <img class="pokemon-img-small" src="${onePokemon['sprites']['other']['dream_world']['front_default']}">
+            </div>
+        </div>
+        `;
+    addTypesOfPokemon(i);
+    addBackgroundToCard(i);
     }
 }
 
 
-function renderOnePokemon(i, pokemonData) {
-    document.getElementById('container').innerHTML += /*html*/`
-        <div id="pokemon-card-small${i}" class="pokemon-card-small bg-small">
-            <div class="pokemon-card-top">
-                <h2 class="fit">${pokemonData['name']}</h2>
-                <span class="fit pokemon-id">#${pokemonData['id']}</span>
-            </div>
-            <div class="pokemon-card-bottom">
-                <div id="pokemon-types${i}" class="pokemon-types fit"></div>
-                <img class="pokemon-img-small" src="${pokemonData['sprites']['other']['dream_world']['front_default']}">
-            </div>
-        </div>
-        `;
-    addTypesOfPokemon(i, pokemonData);
-    addBackgroundToCard(i, pokemonData);
-}
-
-
-function addTypesOfPokemon(i, pokemonData) {
-    for (let t = 0; t < pokemonData['types'].length; t++) {
-        let type = pokemonData['types'][t]['type']['name'];
+function addTypesOfPokemon(i) {
+    for (let t = 0; t < pokemonData[i]['types'].length; t++) {
+        let type = pokemonData[i]['types'][t]['type']['name'];
         document.getElementById(`pokemon-types${i}`).innerHTML += /*html*/`
             <span class="fit">${type}</span>
             `;
@@ -56,27 +67,17 @@ function addTypeColor(i) {
 }
 
 
-async function addBackgroundToCard(i, pokemonData) {
-    let url = pokemonData['species']['url'];
-    let response = await fetch(url);
-    let pokemonSpecies = await response.json();
-    let color = pokemonSpecies['color']['name'];
+async function addBackgroundToCard(i) {
+    let color = pokemonSpecies[i]['color']['name'];
     document.getElementById(`pokemon-card-small${i}`).classList.add(`bg-small-${color}`);
 }
 
 
 function nextPokemons() {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        document.getElementById('b').classList.add('n-scroll');
         offset += LIMIT;
-        LIMIT = 5;
-        document.getElementById('body').classList.add('n-scroll');
-        // document.getElementById('loading').classList.remove('d-none');
+        LIMIT = 3;
         init();
-        setTimeout(remove, 1000);
     }
-}
-
-function remove() {
-    document.getElementById('body').classList.remove('n-scroll');
-    // document.getElementById('loading').classList.add('d-none');
 }
