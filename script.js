1ee94a1f3da10d1d2f93dcfd681751999b4fd232
Lukas Kolbe <kolbe.lukas@gmx.de>
2022-07-12 22:31:26 +0200

    performance problem

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let currentPokemon;
+let pokemonData = [];
+let LIMIT = 50;
+let offset = 0;
 
 async function init() {
     let url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
     let response = await fetch(url);
     let allPokemon = await response.json();
-    loadPokemonNames(allPokemon);
+    loadPokemonData(allPokemon);
 }
 
 
-async function loadPokemonNames(allPokemon) {
-    for (let i = 0; i < 20 /*allPokemon['results'].length*/; i++) {
+async function loadPokemonData(allPokemon) {
+    for (let i = offset; i < offset + LIMIT; i++) {
         let url = allPokemon['results'][i]['url'];
         let response = await fetch(url);
         let pokemonData = await response.json();
@@ -60,4 +62,21 @@ async function addBackgroundToCard(i, pokemonData) {
     let pokemonSpecies = await response.json();
     let color = pokemonSpecies['color']['name'];
     document.getElementById(`pokemon-card-small${i}`).classList.add(`bg-small-${color}`);
+}
+
+
+function nextPokemons() {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        offset += LIMIT;
+        LIMIT = 5;
+        document.getElementById('body').classList.add('n-scroll');
+        // document.getElementById('loading').classList.remove('d-none');
+        init();
+        setTimeout(remove, 1000);
+    }
+}
+
+function remove() {
+    document.getElementById('body').classList.remove('n-scroll');
+    // document.getElementById('loading').classList.add('d-none');
 }
